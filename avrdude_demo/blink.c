e70b9a0ee6650d66394a4a9e528b91384dcd14d8
Komel Merchant <komelm13@gmail.com>
2017-02-20 23:54:08 -0500

    added manual flash programming demo

---
+++ avrdude_demo/blink.c
@@ -0,0 +1,22 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define BLINK_DELAY_MS 100
+
+int main(void) {
+    //Set pin 5 @ output
+    DDRB |= _BV(DDB5);
+    
+    while(1) {
+        
+        //Switch on light
+        PORTB |= _BV(PORTB5);
+        _delay_ms(BLINK_DELAY_MS);
+    
+        //Swithc off light
+        PORTB &= ~_BV(PORTB5);
+        _delay_ms(BLINK_DELAY_MS);
+
+    }
+
+}
