3cfd4d79eadf575fc494de1fe2a4c0a6d3001d52
Komel Merchant <komelm13@gmail.com>
2017-02-21 02:34:51 -0500

    new commit

--- avrdude_demo/blink.c
+++ avrdude_demo/blink.c
@@ -1,20 +1,19 @@
 #include <avr/io.h>
 #include <util/delay.h>
-
 #define BLINK_DELAY_MS 100
 
 int main(void) {
     //Set pin 5 @ output
-    DDRB |= _BV(DDB5);
+    DDRB |= _BV(DDB4);
     
     while(1) {
         
         //Switch on light
-        PORTB |= _BV(PORTB5);
+        PORTB |= _BV(PORTB4);
         _delay_ms(BLINK_DELAY_MS);
     
         //Swithc off light
-        PORTB &= ~_BV(PORTB5);
+        PORTB &= ~_BV(PORTB4);
         _delay_ms(BLINK_DELAY_MS);
 
     }
